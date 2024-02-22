1173e09fffa93d227f55a8bf1e25d928d26c39ee
Ravitejakamineni <160726010+Ravitejakamineni@users.noreply.github.com>
2024-02-22 11:08:58 +0530

    Add files via upload

---
+++ find all unique words.py
@@ -0,0 +1,14 @@
+def pairs-with_sum(lst,g_sum):
+    complement_dict={}
+     pairs=[]
+    for num in 1st:
+        complement=g-sum-num
+        is complement in complement_dict:
+            pairs.append((num,complement))
+            else:
+                complement_dict[num]=complement
+                return pairs
+            nums=[1,2,3,4,5,6,7,8,9]
+            val=10
+            print("list all pairs of the said list whose sum equals to",val)
+            print(pairs_with.sum(nums,val))
